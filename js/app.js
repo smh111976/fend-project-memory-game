f2f74837ec42501ff4b8513c420eda65808f462f
Scott Henning <scott@scotthenning.info>
2018-05-07 16:08:41 -0500

    added 'deal' function to lay out cards at beginning of game and when restart button is clicked

--- js/app.js
+++ js/app.js
@@ -7,20 +7,17 @@
    {suit: "cube"}, {suit: "cube"}, {suit: "leaf"}, {suit: "leaf"},
    {suit: "bicycle"}, {suit: "bicycle"}, {suit: "bomb"}, {suit: "bomb"}
  ];
+ // deck table
+ const deckTable = document.querySelector('section.deck');
+ // new array for the shuffled deck
  let shuffledDeck = [];
+ // restart button
  const restart = document.querySelector('.restart');
+ // Restart button click event listener
  restart.addEventListener('click', function(e) {
-   shuffledDeck = shuffle(deck);
-   const fragment = document.createDocumentFragment();
-   for(const card of shuffledDeck) {
-     const newCard = document.createElement('div');
-     const cardFront = document.createElement('div');
-     const cardBack = document.createElement('div');
-     let frontCls = ["foo", "bar"];
-     cardFront.classList.add(...frontCls);
-     console.log(cardFront.outerHTML);
-   }
- })
+   deal(deck);
+ });
+
 
 
 /*
@@ -29,6 +26,7 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+ deal(deck);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -45,6 +43,32 @@ function shuffle(array) {
     return array;
 }
 
+// Lay Out the Deck function
+function deal(deck) {
+  // shuffle deck of cards
+  shuffledDeck = shuffle(deck);
+  const fragment = document.createDocumentFragment();
+  for(const card of shuffledDeck) {
+    const newCard = document.createElement('div');
+    const cardFront = document.createElement('div');
+    const cardBack = document.createElement('div');
+    const cardIcon = document.createElement('i');
+    const cardFrontCls = ["card__side", "card__side--front"];
+    const cardBackCls = ["card__side", "card__side--back"];
+    let iconCls = ["fa", "fa-" + card.suit];
+    newCard.classList.add('card');
+    newCard.appendChild(cardFront);
+    newCard.appendChild(cardBack);
+    cardFront.classList.add(...cardFrontCls);
+    cardBack.classList.add(...cardBackCls);
+    cardIcon.classList.add(...iconCls);
+    cardBack.appendChild(cardIcon);
+    fragment.appendChild(newCard);
+  }
+  deckTable.innerHTML = "";
+  deckTable.appendChild(fragment);
+}
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -56,3 +80,10 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ deckTable.addEventListener('click', function(e) {
+   e.target.classList.add('open');
+   if(e.target.classList.contains('card')) {
+     e.target.classList.add('open');
+     console.log('is card');
+   }
+ });
